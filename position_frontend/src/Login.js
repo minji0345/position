1e63aab7a5b6a050d734ecd49d8bbc0dde34841f
Minji Kim <minji1004y@gmail.com>
2021-02-04 04:27:14 +0900

    In progress route

---
+++ position_frontend/src/Login.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { Switch, Link, Route, BrowserRouter as Router } from "react-router-dom"
+import App from './App'
+// import logo from './compoenets/images/logo-02.png';
+import './App.css';
+// import Main from './compoenets/Main';
+// import HeaderBar from './compoenets/HeaderBar';
+
+function Login() {
+
+
+    return (
+        <Router>
+            <Link to="/">
+                <button>main</button>
+            </Link>
+            <hr />
+            <Switch>
+                <Route exact path="/" component={App} />
+            </Switch>
+        </Router>
+        );
+    }
+    
+export default Login;
