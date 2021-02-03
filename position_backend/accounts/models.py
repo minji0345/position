e0b6410997cf78a482b3e3776ca88098295dcd6d
kjw7953 <uis7953@naver.com>
2021-02-04 03:29:52 +0900

    add some fields to accounts/models.py
    1. add is_active fields to represent whether that instance is active
    2. add deleted_at fields to represent when that instance's active field become false

--- position_backend/accounts/models.py
+++ position_backend/accounts/models.py
@@ -6,38 +6,44 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	introduction = models.TextField(default="")
-	tasks = models.ManyToManyField('taskManager.Task', related_name='users')
-	created_at = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    introduction = models.TextField(default="")
+    is_active = models.BooleanField(default=True)
+    tasks = models.ManyToManyField('taskManager.Task', related_name='users')
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True)
 
-	def __str__(self):
-		return 'introduction: %s' % (self.introduction)
+    def __str__(self):
+        return 'user: %s, introduction: %s' % (self.user.username, self.introduction)
 
-	@receiver(post_save, sender=User)
-	def create_user_profile(sender, instance, created, **kwargs):
-		if created:
-			Profile.objects.create(user=instance)
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
 
-	@receiver(post_save, sender=User)
-	def save_user_profile(sender, instance, **kwargs):
-		instance.profile.save()
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 
 class Team(models.Model):
-	name = models.CharField(max_length=30)
-	tag_color = models.CharField(max_length=10)
-	team_info = models.TextField(default="")
-	created_at = models.DateTimeField(auto_now_add=True)
-	users = models.ManyToManyField(Profile, through='TeamLog', related_name='teams')
+    name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    tag_color = models.CharField(max_length=10)
+    team_info = models.TextField(default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True)
+    users = models.ManyToManyField(Profile, through='TeamLog', related_name='teams')
 
-	def __str__(self):
-		return 'name: %s, color: %s, created_at: %s' % (self.name, self.tag_color, self.created_at)
+    def __str__(self):
+        return 'name: %s, color: %s, created_at: %s' % (self.name, self.tag_color, self.created_at)
 
 
 class TeamLog(models.Model):
-	user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-	team = models.ForeignKey(Team, on_delete=models.CASCADE)
-	username = models.CharField(max_length=20, default="", blank=True)
-	date_joined = models.DateTimeField(auto_now_add=True)
-	date_quitted = models.DateTimeField(null=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    date_quitted = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return 'user: %s, team: %s' % (self.user.user.username, self.team.name)
